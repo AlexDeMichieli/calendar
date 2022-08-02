c0d6ec9a505ab577a9b229b5f5a40e80d66961c6
Alexander <alexdemichieli@github.com>
2022-08-02 10:49:16 -0700

    prefill feature almost completed

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,74 +1,86 @@
-import React from 'react'
-import { useState } from 'react'
-import moment from 'moment'
-
+import React from "react";
+import { useState, useEffect } from "react";
+import moment from "moment";
 
 const Grid = ({ allDaysOfMonth, prefillTime }) => {
-  console.log(prefillTime)
+  const [timeIn, setTimeIn] = useState({});
+  const [timeOut, setTimeOut] = useState({});
+  //console.log(prefillTime)
 
-  const [timeIn, setTimeIn] = useState({})
-  const [timeOut, setTimeOut] = useState({})
+  useEffect(() => {
+    //console.log(allDaysOfMonth)
+    allDaysOfMonth.map((item, index) => {
+      setTimeIn((state) => {
+        return {
+          ...state,
+          [index]: `${item} ${prefillTime.timeIn}`,
+        };
+      });
 
-  const computeHours = (timeIn, timeOut, cell) =>{
+      setTimeOut((state) => {
+        return {
+          ...state,
+          [index]: `${item} ${prefillTime.timeOut}`,
+        };
+      });
+    });
+  }, [prefillTime]);
+  console.log(timeIn);
+
+  const computeHours = (timeIn, timeOut, cell) => {
     let start = moment(timeIn[cell]);
     let end = moment(timeOut[cell]);
     let diff = end.diff(start);
-    let computedHours = moment.utc(diff).format('H.mm')
+    let computedHours = moment.utc(diff).format("H.mm");
 
-    return computedHours
-  }
+    return computedHours;
+  };
 
   const calculateTime = () => {
-    const total = []
-    let sum = 0
+    const total = [];
+    let sum = 0;
 
     Object.keys(timeIn).forEach(function (key) {
-
-      let sumOfRowHours = computeHours(timeIn, timeOut, key)
-      total.push(parseFloat(sumOfRowHours))
-
+      let sumOfRowHours = computeHours(timeIn, timeOut, key);
+      total.push(parseFloat(sumOfRowHours));
     });
 
     for (let i = 0; i < total.length; i++) {
-      console.log(total[i])
-      sum += total[i]
+      console.log(total[i]);
+      sum += total[i];
     }
 
-    return sum
-  }
+    return sum;
+  };
 
-  const enterTimeIn = (e, cell, day) => {
-    console.log(e, cell, day)
+  const enterTimeIn = (e, cell, day, preFillDates) => {
     setTimeIn((state) => {
       return {
         ...state,
-        [cell]: `${day} ${e.target.value}`
+        [cell]: `${day} ${e.target.value}`,
       };
     });
-  }
+  };
 
   const enterTimeOut = (e, cell, day) => {
     setTimeOut((state) => {
       return {
         ...state,
-        [cell]: `${day} ${e.target.value}`
+        [cell]: `${day} ${e.target.value}`,
       };
     });
-  }
+  };
 
-  const enterNotes = () => {
-
-  }
-
+  const enterNotes = () => {};
 
   const isWeekend = (day) => {
-    let currentDay = moment(day).format('LLLL')
+    let currentDay = moment(day).format("LLLL");
     if (currentDay.includes("Sat") || currentDay.includes("Sun")) {
-      return true
+      return true;
     }
-  }
+  };
   return (
-    <div className='container'>
+    <div className="container">
       <table className="table">
         <thead className="thead-dark">
           <tr>
@@ -80,25 +92,62 @@ const Grid = ({ allDaysOfMonth, prefillTime }) => {
           </tr>
         </thead>
         <tbody>
-          {allDaysOfMonth && allDaysOfMonth.map((day, cell) => {
-       
-            return (
-              <tr key={cell}>
-                <th scope="row">{day}</th>
-                <td className='timeIn' style={{ background: isWeekend(day) ? 'grey' : 'white' }}>{!isWeekend(day) && <input onChange={(e) => enterTimeIn(e, cell, day)} defaultValue={prefillTime.timeIn} className="form-control" type="text" placeholder="Time In" />}</td>
-
-                <td className='timeOut' style={{ background: isWeekend(day) ? 'grey' : 'white' }}>{!isWeekend(day) && <input onChange={(e) => enterTimeOut(e, cell, day)} className="form-control" type="text" placeholder="Time Out" />}</td>
-
-                <td className='hours' style={{ background: isWeekend(day) ? 'grey' : 'white' }}>{!isWeekend(day) && computeHours(timeIn, timeOut, cell)}</td>
-                <td className='notes' style={{ background: isWeekend(day) ? 'grey' : 'white' }}>{!isWeekend(day) && <input onChange={(e) => enterNotes(e, cell)} className="form-control" type="text" placeholder="Notes" />}</td>
-              </tr>
-            )
-          })}
+          {allDaysOfMonth &&
+            allDaysOfMonth.map((day, cell) => {
+              return (
+                <tr key={cell}>
+                  <th scope="row">{day}</th>
+                  <td style={{ background: isWeekend(day) ? "grey" : "white" }}>
+                    {!isWeekend(day) && (
+                      <input
+                        className="timeIn"
+                        onChange={(e) => enterTimeIn(e, cell, day)}
+                        defaultValue={timeIn[cell]}
+                        className="form-control"
+                        type="text"
+                        placeholder="Time In"
+                      />
+                    )}
+                  </td>
+                  <td style={{ background: isWeekend(day) ? "grey" : "white" }}>
+                    {!isWeekend(day) && (
+                      <input
+                        className="timeOut"
+                        onChange={(e) => enterTimeOut(e, cell, day)}
+                        defaultValue={timeOut[cell]}
+                        className="form-control"
+                        type="text"
+                        placeholder="Time Out"
+                      />
+                    )}
+                  </td>
+                  <td
+                    className="hours"
+                    style={{ background: isWeekend(day) ? "grey" : "white" }}
+                  >
+                    {!isWeekend(day) && computeHours(timeIn, timeOut, cell)}
+                  </td>
+                  <td
+                    className="notes"
+                    style={{ background: isWeekend(day) ? "grey" : "white" }}
+                  >
+                    {!isWeekend(day) && (
+                      <input
+                        onChange={(e) => enterNotes(e, cell)}
+                        className="form-control"
+                        type="text"
+                        placeholder="Notes"
+                      />
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
-      <h4 className='text-center'>Sum: {calculateTime()}</h4>
+      <h4 className="text-center">Sum: {calculateTime()}</h4>
     </div>
-  )
-}
+  );
+};
 
-export default Grid
+export default Grid;
